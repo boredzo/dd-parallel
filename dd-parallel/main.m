b2b8f94af3333ff0467b1783f53bbb96882ccb97
Peter Hosey <git@boredzo.org>
2014-11-02 09:25:32 -0800

    Initial Commit

---
+++ dd-parallel/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  dd-parallel
+//
+//  Created by Peter Hosey on 2014-11-02.
+//  Copyright (c) 2014 Peter Hosey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
